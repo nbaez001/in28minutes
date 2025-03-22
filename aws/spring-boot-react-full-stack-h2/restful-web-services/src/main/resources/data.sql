7a50183dc057f16db85566063eb43b4e7b004e58
nbaez001 <nbaez001@gmail.com>
2025-03-22 18:37:44 -0500

    Actualizacion

---
+++ aws/spring-boot-react-full-stack-h2/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE)
+VALUES (1, 'in28minutes', '$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','ROLE_USER');
+INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) 
+VALUES (2, 'in28minutes2', '$2a$10$i9AckmxMkb4yKtLCdxeQheCm2pXWB3qZ2G189/Ph/DUci1DvLO.Rq','ROLE_USER');
+
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'in28minutes', 'Learn JPA', sysdate(), false);
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'in28minutes', 'Learn Data JPA', sysdate(), false);
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'in28minutes', 'Learn Microservices', sysdate(), false);

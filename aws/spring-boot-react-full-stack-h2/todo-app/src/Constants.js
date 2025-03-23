0019b00021292d1eb9e6e5a7f7a03293cdc1f40c
nbaez001 <nbaez001@gmail.com>
2025-03-22 20:36:56 -0500

    cambios

--- aws/spring-boot-react-full-stack-h2/todo-app/src/Constants.js
+++ aws/spring-boot-react-full-stack-h2/todo-app/src/Constants.js
@@ -1,4 +1,4 @@
 /* For Best Practices https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables*/
-export const API_URL = 'http://localhost:5000'
-//export const API_URL = 'http://rest-api-in28minutes-qa.kvajmpmiiu.us-east-1.elasticbeanstalk.com'
+//export const API_URL = 'http://localhost:5000'
+export const API_URL = 'http://web-application-in28minutes-env-qa.eba-z2cp2kpu.us-east-2.elasticbeanstalk.com'
 export const JPA_API_URL = `${API_URL}/jpa`
